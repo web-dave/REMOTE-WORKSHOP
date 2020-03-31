88427a286423b8c6ccd73a312a01ac4e91f91fd8
Dave <dave@webdave.de>
2020-03-31 11:59:27 +0200

    mouse-cursor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
+import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
+import { TitleBoxComponent } from './title-box/title-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    InfoBoxComponent
+    InfoBoxComponent,
+    MouseCursorComponent,
+    TitleBoxComponent
   ],
   imports: [
     BrowserModule,
