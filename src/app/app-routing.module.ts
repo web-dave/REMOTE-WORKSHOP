93c58fcd2e1d9f36c8ff7fe60e28c1308720d435
Dave <dave@webdave.de>
2020-04-02 12:07:05 +0200

    set about to default to show guards are not safe!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/books',
+    redirectTo: '/about',
     pathMatch: 'full'
   },
   {
