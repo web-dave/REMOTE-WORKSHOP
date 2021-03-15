ed07385f6051f244611b682a8daacaf4e1ad95db
web-dave <dave@webdave.de>
2021-03-15 14:06:13 +0100

    propperty-event-Binding

--- src/app/in-out/in-out.component.ts
+++ src/app/in-out/in-out.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ng-ws-in-out',
   templateUrl: './in-out.component.html',
-  styleUrls: ['./in-out.component.scss']
+  styleUrls: ['./in-out.component.scss'],
 })
 export class InOutComponent implements OnInit {
+  isHidden = false;
+  foo: 'bar' | 'baz' = 'bar';
 
-  constructor() { }
+  x = 0;
+  y = 0;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  mouseMovement(event: MouseEvent) {
+    this.x = event.x
+    this.y = event.y
   }
-
 }
