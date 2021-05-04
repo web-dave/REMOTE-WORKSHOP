b9709bcd05f090eef78bbf38483a28435465ad2f
web-dave <dave@webdave.de>
2021-05-04 11:39:14 +0200

    ngIf & ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [AppComponent, TopNavComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'zeiss-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  books;
+  constructor(private service: BookService) {}
+  getKeys(book) {
+    return Object.keys(book);
   }
-
+  ngOnInit(): void {
+    // this.service.getBooks().subscribe({
+    //   next: (data) => (this.books = data),
+    //   error: (err) => console.error(err),
+    //   complete: () => console.info('Fertitsch'),
+    // });
+    this.service.getBooks().subscribe(
+      (data) => (this.books = data),
+      (err) => console.error(err),
+      () => console.info('Fertitsch')
+    );
+  }
 }
