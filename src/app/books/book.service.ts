710dffe87d880112988c3b53af4ddb535f8ca8eb
web-dave <dave@webdave.de>
2021-05-05 16:46:04 +0200

    build

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, pluck, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { IBook } from './ibook.interface';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { IBook } from './ibook.interface';
 })
 export class BookService {
   name = 'Hildegard';
-  private restRoot = 'http://localhost:4730/books';
+  private restRoot = environment.url;
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<IBook[]> {
--- src/app/preload.strategy.ts
+++ src/app/preload.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 interface NetworkInformation {
   downlink: number;
   effectiveType: '4g' | '2g' | '3g';
@@ -16,7 +17,8 @@ export class NetworkPreload implements PreloadingStrategy {
   preload(route: Route, fn: () => Observable<any>): Observable<any> {
     const network: NetworkInformation = navigator['connection'];
     console.log(network.effectiveType);
-    if (network.effectiveType === '2g') {
+    console.log('!!! =>', fn);
+    if (network.effectiveType === '4g') {
       return fn();
     } else {
       return of(null);
