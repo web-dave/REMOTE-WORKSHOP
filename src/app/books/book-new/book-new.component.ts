ce4bb8096d8d005b2d1f6271612e4b6e2df3eff4
web-dave <dave@webdave.de>
2021-06-21 14:50:55 +0200

    formArray

--- src/app/books/book-new/book-new.component.ts
+++ src/app/books/book-new/book-new.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { map } from 'rxjs/operators';
 import { IBook } from '../shared/book.interface';
 import { BooksService } from '../shared/books.service';
 
@@ -10,6 +17,7 @@ import { BooksService } from '../shared/books.service';
 })
 export class BookNewComponent implements OnInit {
   bookNewForm: FormGroup;
+  authors: FormArray = new FormArray([]);
   constructor(private builder: FormBuilder, private service: BooksService) {}
 
   ngOnInit(): void {
@@ -26,9 +34,23 @@ export class BookNewComponent implements OnInit {
         url: '',
       }),
     });
+    this.authors.valueChanges
+      .pipe(map((autoren) => autoren.filter((author: string[]) => author)))
+      .subscribe((data) => {
+        console.log(data);
+        this.bookNewForm.get('author').setValue(data.join(','));
+      });
   }
 
   save() {
     this.service.createBook(this.bookNewForm.value).subscribe();
+  }
+
+  addAuthor() {
+    this.authors.push(new FormControl());
+  }
+
+  deleteAuthor(i: number) {
+    this.authors.removeAt(i);
   }
 }
