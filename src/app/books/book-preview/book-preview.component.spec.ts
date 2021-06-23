535cb34ab6ad6359a82909349f7d19df514f38bd
web-dave <dave@webdave.de>
2021-06-23 14:26:35 +0200

    first test

--- src/app/books/book-preview/book-preview.component.spec.ts
+++ src/app/books/book-preview/book-preview.component.spec.ts
@@ -1,25 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { IBook } from '../shared/book.interface';
 import { BookPreviewComponent } from './book-preview.component';
 
-describe('BookPreviewComponent', () => {
-  let component: BookPreviewComponent;
-  let fixture: ComponentFixture<BookPreviewComponent>;
+const book: IBook = {
+  title: 'Design Patterns',
+  subtitle: 'Elements of Reusable Object-Oriented Software',
+  isbn: '978-0-20163-361-0',
+  abstract:
+    'Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.',
+  numPages: 395,
+  author: 'Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides',
+  publisher: {
+    name: 'Addison-Wesley',
+    url: 'http://www.addison-wesley.de/',
+  },
+  cover: 'http://localhost:4730/covers/978-0-20163-361-0.jpg',
+};
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BookPreviewComponent ]
-    })
-    .compileComponents();
-  }));
+fdescribe('BookPreviewComponent', () => {
+  let spectator: Spectator<BookPreviewComponent>;
+  const createComponent = createComponentFactory(BookPreviewComponent);
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BookPreviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have a title', () => {
+    spectator.setInput('book', book);
+    expect(spectator.query('li')).toHaveText(book.title);
   });
 });
