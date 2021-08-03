4f72bb78ca8e7ed89949a76e60ad6fdf824c6408
web-dave <dave@webdave.de>
2021-08-03 15:47:45 +0200

    TS rampup

---
+++ src/app/test1.ts
@@ -0,0 +1,25 @@
+import { getName } from "./test2";
+
+var foo = 1;
+if(true){
+    foo = 1;
+}
+
+let huhu = {name: 'Hurz'}
+
+function bar(val:number): boolean | number{
+    if(val <= 7){
+        return true
+    } else {
+        return 22
+    }
+}
+
+const word = `Der wert ist ${huhu.name}`
+huhu.name = 'Das Lamm'
+
+const root = 'https://kjdhflskj.fv/ödslfikhdjafpö'
+let url = `${root}/6546` // root + '/6546'
+
+getName()
+[].splice()
---
+++ src/app/test2.ts
@@ -0,0 +1,5 @@
+const name = 'kjhkh'
+export const getName = () => this.name
+export function getNames(){
+    let name = 'kljhö' 
+    return this.name}
