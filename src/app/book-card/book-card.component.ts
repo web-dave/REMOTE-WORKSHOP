20a7bbf0eb0087373a583496aa489dcec7ad789c
web-dave <dave@webdave.de>
2021-09-02 09:27:07 +0200

    property binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent implements OnInit {
+  color = 'hotpink';
+  styles = {
+    textShadow: '2pt 2pt #456789',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
