211b2e86efbff85626ae0176d4e9f0daf0e0b27e
web-dave <dave@webdave.de>
2021-10-29 12:37:58 +0200

    tag 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IBook } from './book.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,25 @@ export class AppComponent {
   title = ' bookmonkey-client';
   foo = '🐵';
   bar = 'pojpjo';
+  books: IBook[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
+
+  showDetails(data: IBook) {
+    console.table(data);
+  }
 }
--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  Sanitizer,
+} from '@angular/core';
+import { IBook } from '../book.interface';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent implements OnInit {
+  @Input() content: IBook = { abstract: '', title: '', author: '' };
+  @Output() navigateTo = new EventEmitter<IBook>();
+  x: number | undefined;
 
-  constructor() { }
+  styling = {
+    color: 'purple',
+  };
+
+  btn = '<button>click</button>';
+
+  myClass = true;
+
+  constructor(private snitizer: Sanitizer) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      // this.content.title = 'öxkjvhödjfhka';
+    }, 1500);
   }
 
+  handleDetailClick(event: MouseEvent) {
+    console.log('EVNET:', event);
+    this.x = event.x;
+    this.navigateTo.emit(this.content);
+  }
 }
