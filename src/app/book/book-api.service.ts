a83bdfe86b82eb2055470acc2e398d69ba99cfe3
web-dave <dave@webdave.de>
2022-01-17 09:25:15 +0100

    Service

---
+++ src/app/book/book-api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { IBook } from './book.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookApiService {
+  private books: IBook[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
+  constructor() {}
+
+  getBooks(): IBook[] {
+    return this.books;
+  }
+}
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookApiService } from './book-api.service';
 import { IBook } from './book.interface';
 
 @Component({
@@ -8,24 +9,10 @@ import { IBook } from './book.interface';
 })
 export class BookComponent implements OnInit {
   search = '';
-  books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
-  constructor() {}
+  books: IBook[] = [];
+  constructor(private service: BookApiService) {
+    this.books = this.service.getBooks();
+  }
 
   ngOnInit(): void {}
 
