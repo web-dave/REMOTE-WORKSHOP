860eba85db604fdcd7cfaef8096dc3429b3fa5c4
web-dave <dave@webdave.de>
2022-04-07 10:27:48 +0200

    attr binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent implements OnInit {
+  areaRole = 'navigation';
+  customStyles = {
+    backgroundColor: 'hotpink',
+    color: 'purple',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
