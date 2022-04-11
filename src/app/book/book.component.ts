536e9e5755d90e022290f603d00cffebb3018d3e
web-dave <dave@webdave.de>
2022-04-11 12:29:45 +0200

    async pipe

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { NEVER, Observable, Subscription } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 import { IBook } from 'src/models/book.interface';
 import { BookService } from './book.service';
 
@@ -9,30 +9,14 @@ import { BookService } from './book.service';
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
 })
-export class BookComponent implements OnInit, OnDestroy {
+export class BookComponent implements OnInit {
   searchTerm = '';
-  books: IBook[] = [];
-  tm: any;
-  sub = new Subscription();
-  kill$ = new EventEmitter();
+  books$: Observable<IBook[]> = NEVER;
 
   constructor(private service: BookService) {}
-  ngOnDestroy(): void {
-    clearTimeout(this.tm);
-    this.sub.unsubscribe();
-    this.kill$.emit();
-  }
 
   ngOnInit(): void {
-    this.tm = setTimeout(() => console.log('ölkydjösdaj'), 2000);
-    this.sub.add(
-      this.service.getBooks().subscribe({ next: (data) => (this.books = data) })
-    );
-
-    this.service
-      .getBooks()
-      .pipe(takeUntil(this.kill$))
-      .subscribe({ next: (data) => (this.books = data) });
+    this.books$ = this.service.getBooks();
   }
 
   goToBook(e: IBook) {
--- src/app/book/shared/book-search.pipe.ts
+++ src/app/book/shared/book-search.pipe.ts
@@ -6,7 +6,10 @@ import { IBook } from 'src/models/book.interface';
   pure: true,
 })
 export class BookSearchPipe implements PipeTransform {
-  transform(books: IBook[], searchTerm: string): IBook[] {
+  transform(books: IBook[] | null, searchTerm: string): IBook[] {
+    if (!books) {
+      books = [];
+    }
     return searchTerm === ''
       ? books
       : books.filter(
