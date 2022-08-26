eeaab184a70d702706a5b649c6e6442416166d1f
web-dave <dave@webdave.de>
2022-08-26 12:43:03 +0200

    Lazyloading

---
+++ src/app/book/book-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookComponent } from './book.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookComponent,
+  },
+  {
+    path: ':isbn',
+    component: BookDetailsComponent,
+  },
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class BookRoutingModule {}
--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -4,10 +4,16 @@ import { BookComponent } from './book.component';
 import { BookCardComponent } from './book-card/book-card.component';
 import { FilterBooksPipe } from './filter-books.pipe';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookRoutingModule } from './book-routing.module';
 
 @NgModule({
-  declarations: [BookComponent, BookCardComponent, FilterBooksPipe, BookDetailsComponent],
-  imports: [CommonModule],
+  declarations: [
+    BookComponent,
+    BookCardComponent,
+    FilterBooksPipe,
+    BookDetailsComponent,
+  ],
+  imports: [CommonModule, BookRoutingModule],
   exports: [BookComponent],
 })
 export class BookModule {}
