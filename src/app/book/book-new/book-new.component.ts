61a5228af326f626615386da10747588bf74efac
web-dave <dave@webdave.de>
2022-09-21 12:38:05 +0200

    Validator

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { IBook } from '../book.interface';
-
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-book-new',
   templateUrl: './book-new.component.html',
@@ -9,7 +13,7 @@ import { IBook } from '../book.interface';
 })
 export class BookNewComponent implements OnInit {
   form = this.builder.group<any>({
-    title: ['', [Validators.required, Validators.minLength(7)]],
+    title: ['', [Validators.required, Validators.minLength(7), titleValidator]],
     author: ['Author', []],
     abstract: ['Abstract', []],
     subtitle: ['Subtitle', []],
@@ -19,9 +23,21 @@ export class BookNewComponent implements OnInit {
     price: ['1,99', []],
     cover: ['', []],
   });
+
   constructor(private builder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form.controls.title.valueChanges.subscribe((data) => {
+      if (data === 'Marion') {
+        this.form.controls.subtitle.setValue('Marions Welt');
+        this.form.controls.cover.addValidators(Validators.required);
+        this.form.controls.cover.updateValueAndValidity();
+      } else {
+        this.form.controls.cover.clearValidators();
+        this.form.controls.cover.updateValueAndValidity();
+      }
+    });
+  }
 
   addCtrl() {
     this.form.addControl('foo', new FormControl(['Bar']));
@@ -29,5 +45,13 @@ export class BookNewComponent implements OnInit {
 
   save() {
     console.table(this.form.value);
+  }
+}
+
+function titleValidator(control: AbstractControl): ValidationErrors | null {
+  if (control.value === 'Marion') {
+    return null;
+  } else {
+    return { title: 'Title is not Marion' };
   }
 }
