cce06557db789f8f1dae1afc1bc2e753682435ae
web-dave <dave@webdave.de>
2023-03-01 15:36:15 +0100

    APP_INITIALIZER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -7,11 +7,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 
 import { AboutModule } from './about/about.module';
+APP_INITIALIZER;
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, AboutModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        // get data
+      },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
