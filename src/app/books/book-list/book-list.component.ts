7c61190d24c4e05eb56d52c52110c73eee936cbf
web-dave <dave@webdave.de>
2023-03-01 15:33:26 +0100

    clean up

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -9,12 +9,10 @@ import { BookService } from '../book.service';
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent {
   private bookService = inject(BookService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-
-  books: IBook[] = [];
   books$: Observable<IBook[]> = this.bookService.getAll();
 
   @Input()
@@ -24,26 +22,5 @@ export class BookListComponent implements OnInit {
     console.log(e);
     this.router.navigate([e.isbn], { relativeTo: this.route });
     //   this.error = Boolean((e.target as HTMLInputElement).value);
-  }
-
-  ngOnInit() {
-    console.log(this.route);
-    const foo = {
-      value: 'bar',
-    };
-    foo.value;
-    const key = 'value';
-    foo[key];
-    // Only next signal
-    // this.bookService.getAll().subscribe((data) => (this.books = data));
-
-    // this.bookService.getAll().subscribe({
-    //   next: (data) => (this.books = data),
-    //   error: (err) => {
-    //     console.error(err);
-    //     this.error = true;
-    //   },
-    //   complete: () => console.info('DONE!!'),
-    // });
   }
 }
