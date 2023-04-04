05267f1d57b22bf7c40138b8b46a115e697f5e97
web-dave <dave@webdave.de>
2023-04-04 16:36:43 +0200

    Childrouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { BooksComponent } from './books/books.component';
 
 const routes: Routes = [
-  {
-    path: 'books',
-    component: BooksComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-  },
   {
     path: '',
     redirectTo: '/books',
