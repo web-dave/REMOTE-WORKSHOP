6ffd1cfd39f667ff23da51a46c3c63e50c0feca1
Dave <dave@webdave.de>
2024-10-22 16:05:23 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { AboutComponent } from './about/about.component';
+import { BookListComponent } from './books/book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    title: 'Book Store',
+    path: 'books',
+    component: BooksComponent,
+    children: [
+      {
+        path: '',
+        component: BookListComponent,
+      },
+    ],
+  },
+  { title: 'who am I', path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
