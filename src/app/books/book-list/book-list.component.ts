c56e6b9fa97aef0dab29079e874ff06a25f5a14b
Dave <dave@webdave.de>
2024-10-23 11:54:07 +0200

    Use Service ngIf ngFor

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  private service = inject(BookService);
+  books: any[] = [];
 
+  ngOnInit(): void {
+    this.service.getAll().subscribe({
+      next: (data) => (this.books = data),
+      complete: () => console.log('Done'),
+    });
+  }
 }
